592bd79e62a814b8bac7d9c9a26e1ff6069bfd02
Ilya Siganov <ilya.blan4@gmail.com>
2015-11-21 23:13:28 +0600

    fix redis config

--- src/main/kotlin/com.seniorsigan.qrauth/config/HttpSessionConfig.kt
+++ src/main/kotlin/com.seniorsigan.qrauth/config/HttpSessionConfig.kt
@@ -1,6 +1,13 @@
 package com.seniorsigan.qrauth.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.session.data.redis.config.ConfigureRedisAction
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 
 @EnableRedisHttpSession
-open class HttpSessionConfig
+open class HttpSessionConfig {
+    @Bean
+    open fun configureRedisAction(): ConfigureRedisAction {
+        return ConfigureRedisAction.NO_OP
+    }
+}
